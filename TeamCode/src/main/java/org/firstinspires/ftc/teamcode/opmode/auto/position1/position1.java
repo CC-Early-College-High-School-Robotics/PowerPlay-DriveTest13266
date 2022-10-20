a33ec3e6bfdaea8a94f7779fc01dea2783bf54e8
Justin <you@example.com>
2022-10-19 19:28:42 -0500

    position 1 path first half ready

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/position1/position1.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmode.auto.position1;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
+
+public class position1 extends LinearOpMode {
+    Pose2d startPose = new Pose2d(12, 63.2, Math.toRadians(-90));
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initalize Subsystems
+        MecanumDriveSubsystem drive = new MecanumDriveSubsystem(this);
+
+        /** On initialization **/
+        drive.setPoseEstimate(startPose);
+        /** Open CV **/
+
+        waitForStart();
+        if (isStopRequested()) return;
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d(35, -62, Math.toRadians(90)))
+                .lineToConstantHeading(new Vector2d(20, -60))
+                .splineToConstantHeading(new Vector2d(13, -47), Math.toRadians(90))
+                .splineTo(new Vector2d(8, -32), Math.toRadians(135))
+                .build()
+        );
+    }
+}
